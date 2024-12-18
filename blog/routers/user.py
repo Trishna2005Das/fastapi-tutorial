d1c59e0df7049b828357014252b32b39c3d373b9
Trishna2005Das <trishnadas2005@gmail.com>
2024-12-18 18:14:23 +0530

    removed redundant tags syntax

--- blog/routers/user.py
+++ blog/routers/user.py
@@ -9,14 +9,14 @@ get_db=database.get_db
 router=APIRouter( prefix='/user',
     tags=['users'])
 
-@router.post('/user',response_model=schemas.ShowUser,tags=['users'])
+@router.post('/user',response_model=schemas.ShowUser)
 def create_user(request:schemas.User,db:Session=Depends(get_db)):
    return user.create(request,db)
    
 
 #we need to store in the dadtabase so we need the database connection
 
-@router.get('/user/{id}',response_model=schemas.ShowUser,tags=['users'])
+@router.get('/user/{id}',response_model=schemas.ShowUser)
 def get_user(id:int,db:Session=Depends(get_db)):
    return user.show(id,db)
    
